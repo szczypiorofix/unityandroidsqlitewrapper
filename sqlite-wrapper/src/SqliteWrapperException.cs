f2187e31ee89d395bcb29c1af96cb5f525508457
szczypiorofix <szczypiorofix@o2.pl>
2022-05-03 23:42:12 +0200

    Added own SqliteWrapper Exception

---
+++ sqlite-wrapper/src/SqliteWrapperException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+
+
+namespace sqlite_wrapper {
+
+    class SqliteWrapperException : Exception {
+
+        public SqliteWrapperException( string message ) : base( message ) {
+            Debug.LogError( SQLiteWrapper.DEBUG_NAME + message );
+        }
+
+    }
+
+
+}
